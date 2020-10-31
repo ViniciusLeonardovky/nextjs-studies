756032392488b9d689f9cf9871f247b242e94462
ViniciusLeonadovky <vile.pb@gmail.com>
2020-10-31 01:40:25 -0300

    Prismic implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,18 @@
 import SEO from '@/components/SEO';
+import Link from 'next/link';
+import { client } from '@/lib/prismic';
 import { GetServerSideProps } from 'next';
 import { Title } from '../styles/pages/Home';
+import Prismic from 'prismic-javascript';
+import PrismicDOM from 'prismic-dom';
+import { Document } from 'prismic-javascript/types/documents';
 
-interface IProduct {
-  id: string;
-  title: string;
-}
 
 interface HomeProps {
-  recommendedProducts: IProduct[];
+  recommendedProducts: Document[];
 }
 
 export default function Home({ recommendedProducts }: HomeProps) {
-  async function handleSum() {
-    const math = (await import('../lib/math')).default;
-
-    alert(math.sum(2, 3));
-  }
 
   return (
     <div>
@@ -27,30 +23,32 @@ export default function Home({ recommendedProducts }: HomeProps) {
       />
       <section>
         <Title>Products</Title>
-
         <ul>
           {recommendedProducts.map((recommendedProduct) => {
             return (
-              <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
+              <li key={recommendedProduct.id}>
+                <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                  <a>
+                    {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                  </a>
+                </Link>
+              </li>
             );
           })}
         </ul>
       </section>
-
-      <button onClick={handleSum}>Sum!</button>
     </div>
   );
 }
 
 export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/recommended`
-  );
-  const recommendedProducts = await response.json();
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at('document.type', 'product')
+  ]);
 
   return {
     props: {
-      recommendedProducts,
+      recommendedProducts: recommendedProducts.results,
     },
   };
 };
